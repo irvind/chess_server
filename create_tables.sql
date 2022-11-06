981847b03b084d34b075fa9549edc7301b42bd6a
Eugene Obukhov <evgeniy.d.obukhov@bia-tech.ru>
2022-11-06 21:13:30 +0300

    Update database schema

--- create_tables.sql
+++ create_tables.sql
@@ -1,6 +1,17 @@
 DROP TABLE IF EXISTS games;
+DROP TABLE IF EXISTS players;
+
+CREATE TABLE players (
+    id bigserial PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    auth_secret varchar(255) NOT NULL,
+    created_at timestamp NOT NULL
+);
 
 CREATE TABLE games (
-    id SERIAL PRIMARY KEY,
-    created_at TIMESTAMP
+    id bigserial PRIMARY KEY,
+    created_by bigint NOT NULL,
+    opponent bigint,
+    creator_white boolean,
+    created_at timestamp NOT NULL
 );
