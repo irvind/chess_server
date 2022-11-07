fc14f315a328828c9003665c9302fa1c136953ea
Eugene Obukhov <evgeniy.d.obukhov@bia-tech.ru>
2022-11-08 00:21:49 +0300

    Implement post players api method

--- api/players.go
+++ api/players.go
@@ -3,8 +3,29 @@ package api
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+
+	"github.com/irvind/chess_server/dao"
+	"github.com/google/uuid"
 )
 
+type PostPlayersParams struct {
+	Name string `json:"name" binding:"required,min=2,max=64"`
+}
+
 func postPlayers(c *gin.Context) {
-	c.IndentedJSON(http.StatusCreated, gin.H{"message": "postPlayers"})
+	var params PostPlayersParams
+	if err := c.BindJSON(&params); err != nil {
+		// TODO: return error resp
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	authSecret := uuid.New().String()
+	newPlayer, err := dao.CreatePlayer(params.Name, authSecret)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	c.IndentedJSON(http.StatusCreated, newPlayer)
 }
--- dao/dao.go
+++ dao/dao.go
@@ -1,11 +1,19 @@
 package dao
 
 import (
+	"time"
 	"github.com/irvind/chess_server/database"
 )
 
 type Game struct {
 	ID int64
+}
+
+type Player struct {
+	ID int64
+	Name string
+	AuthSecret string
+	CreatedAt time.Time
 }
 
 func GetGames() ([]Game, error) {
@@ -43,4 +51,24 @@ func CreateGame() (int64, error) {
 	}
 
 	return newId, nil
+}
+
+func CreatePlayer(name string, authSecret string) (*Player, error) {
+	// TODO: remove code dupliation
+	db := database.GetDB()
+	row := db.QueryRow(
+		"INSERT INTO players (name, auth_secret, created_at) VALUES ($1, $2, NOW()) RETURNING id, created_at",
+		name,
+		authSecret,
+	)
+
+	var newID int64
+	var createdAt time.Time
+	if err := row.Scan(&newID, &createdAt); err != nil {
+		return nil, err
+	}
+
+	player := &Player{ID: newID, Name: name, AuthSecret: authSecret, CreatedAt: createdAt}
+
+	return player, nil
 }
